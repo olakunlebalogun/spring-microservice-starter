1874a2154f196d00a45d11a928d9c0403a7f1924
olakunlebalogun <olakunlebalogun247@gmail.com>
2023-02-09 12:14:48 +0100

    closes #1, #2; worked on the product add and list-all

---
+++ src/main/java/com/olakunle/sixhour/product/entity/Product.java
@@ -0,0 +1,27 @@
+package com.olakunle.sixhour.product.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Positive;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@AllArgsConstructor
+@Setter
+@Getter
+@NoArgsConstructor
+@Builder
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String description;
+    @Column(nullable = false, precision = 2)
+    @Positive
+    private BigDecimal price;
+}
