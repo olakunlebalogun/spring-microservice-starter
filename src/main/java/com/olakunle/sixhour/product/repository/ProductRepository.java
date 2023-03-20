770bd698c10d0be959eb963f489b01282f814f4f
olakunlebalogun <olakunlebalogun247@gmail.com>
2023-03-20 18:04:54 +0100

    added tests and switched to multi-module project

--- src/main/java/com/olakunle/sixhour/product/repository/ProductRepository.java
+++ src/main/java/com/olakunle/sixhour/product/repository/ProductRepository.java
@@ -5,10 +5,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-<<<<<<< HEAD
-public interface ProductRepository extends JpaRepository<Product, Integer> {
-=======
 public interface ProductRepository extends MongoRepository<Product, String> {
->>>>>>> 2cb0402 (changes made on this branch)
 
 }
