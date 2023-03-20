7dc98c16b85b7bab6ed82e98dd41be4a633fe57a
olakunlebalogun <olakunlebalogun247@gmail.com>
2023-03-20 18:04:54 +0100

    added tests and switched to multi-module project

--- src/main/java/com/olakunle/sixhour/product/utils/ProductURL.java
+++
@@ -1,7 +0,0 @@
-package com.olakunle.sixhour.product.utils;
-
-public class ProductURL {
-    public static final String PRODUCT= "/api/v1/products";
-    public static final String ADD= "";
-    public static final String FIND_ALL= "";
-}
