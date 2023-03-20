01d44b01c6f1f493c996b35a0b9e24b56bf0cfe5
olakunlebalogun <olakunlebalogun247@gmail.com>
2023-03-20 12:21:09 +0100

    resloved adding product and getting all products with local database #2

--- src/main/java/com/olakunle/sixhour/product/entity/Product.java
+++ src/main/java/com/olakunle/sixhour/product/entity/Product.java
@@ -1,8 +1,8 @@
 package com.olakunle.sixhour.product.entity;
 
-import jakarta.persistence.*;
 import jakarta.validation.constraints.Positive;
 import lombok.*;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.math.BigDecimal;
@@ -12,17 +12,12 @@ import java.math.BigDecimal;
 @Getter
 @NoArgsConstructor
 @Builder
-@Document
+@Document(value = "product")
 public class Product {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String id;
-
-    @Column(nullable = false)
     private String name;
-    @Column(nullable = false)
     private String description;
-    @Column(nullable = false, precision = 10)
     @Positive
     private BigDecimal price;
 }
